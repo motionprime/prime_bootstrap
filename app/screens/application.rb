e58f9d1b2dc424e5a6523c0a5871f12395c50fc2
Iskander Haziev <gvalmon@gmail.com>
2014-02-08 20:30:20 +0400

    update icons

--- app/screens/application.rb
+++ app/screens/application.rb
@@ -2,6 +2,6 @@ class ApplicationScreen < Prime::Screen
   before_render :setup_navigation
 
   def setup_navigation
-    set_navigation_left_button 'menu', image: 'icons/blue/menu.png', action: :toggle_sidebar
+    set_navigation_left_button 'menu', image: 'icons/menu.png', action: :toggle_sidebar
   end
 end
--- app/styles/mixins/icons.rb
+++ app/styles/mixins/icons.rb
@@ -3,9 +3,11 @@ Prime::Styles.define :_mixin do
     width: 28,
     height: 28
 
-  style :ui_icon_home, mixins: [:ui_icon],
-    image: 'icons/blue/home.png'
-
-  style :ui_icon_book, mixins: [:ui_icon],
-    image: 'icons/blue/book.png'
+  %w[
+    arrow_left arrow_right book calendar camera chat_double chat_empty chat
+    cloud download home letter location lock market menu mic phone private
+    profile trash upload video
+  ].each do |icon|
+    style :"ui_icon_#{icon}", mixins: [:ui_icon], image: "icons/#{icon}.png"
+  end
 end
